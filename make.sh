110c5be87d035df243e899623eb329476a45c227
frankmorgner <frankmorgner@bc35da7a-fd73-48a3-9038-9f319fca9165>
2009-10-14 20:21:10 +0000

    Initial commit. Note that this code is not tested with a real implementation of a German elektronischer Personalausweis. And yes, it still contains errors.

---
+++ make.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+wget http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz
+wget "http://marc.info/?l=openssl-dev&m=124340739803855&q=p3" -O ibm3.patch
+tar -xzf openssl-1.0.0-beta2.tar.gz
+
+cd openssl-1.0.0-beta2
+patch -p1 < ../ibm3.patch
+patch -p1 < ../openpace.patch
+ln crypto/cmac/cmac.h include/openssl/
+ln crypto/pace/pace.h include/openssl/
+
+./config
+make
+make -C crypto/pace
+make
