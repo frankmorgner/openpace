a44b27fe7f83d59de33de04367ca9c177c924978
wolf-mueller <wolf-mueller@bc35da7a-fd73-48a3-9038-9f319fca9165>
2009-11-06 14:10:27 +0000

    hard links replaced by symbolic ones.

--- make.sh
+++ make.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# downloading and extracting
 wget http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz
 wget "http://marc.info/?l=openssl-dev&m=125730780821132&q=p3" -O ibm4.patch
 tar -xzf openssl-1.0.0-beta3.tar.gz
 
+# datching
 cd openssl-1.0.0-beta3
 patch --strip 1 < ../ibm4.patch
 patch --strip 1 < ../openpace.patch
-ln crypto/cmac/cmac.h include/openssl/
-ln crypto/pace/pace.h include/openssl/
 
+# symbolic linking
+(cd include/openssl/ ;\
+ln -v -s ../../crypto/cmac/cmac.h \
+         ../../crypto/pace/pace.h \
+         .                    )
+# compile
 sleep 1
 ./config && make
 make -C crypto/pace
