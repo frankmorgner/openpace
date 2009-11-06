dc6cfc3d389a8a19bf0d0c1765a783564a7a90a2
oepen <oepen@bc35da7a-fd73-48a3-9038-9f319fca9165>
2009-11-06 13:48:04 +0000

    - Updated make.sh to use latest versions of openssl and the IBM CMAC patch - Building should now work without errors

--- make.sh
+++ make.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-wget http://www.openssl.org/source/openssl-1.0.0-beta2.tar.gz
-wget "http://marc.info/?l=openssl-dev&m=124340739803855&q=p3" -O ibm3.patch
-tar -xzf openssl-1.0.0-beta2.tar.gz
+wget http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz
+wget "http://marc.info/?l=openssl-dev&m=125730780821132&q=p3" -O ibm4.patch
+tar -xzf openssl-1.0.0-beta3.tar.gz
 
-cd openssl-1.0.0-beta2
-patch -p1 < ../ibm3.patch
-patch -p1 < ../openpace.patch
+cd openssl-1.0.0-beta3
+patch --strip 1 < ../ibm4.patch
+patch --strip 1 < ../openpace.patch
 ln crypto/cmac/cmac.h include/openssl/
 ln crypto/pace/pace.h include/openssl/
 
-./config
-make
+sleep 1
+./config && make
 make -C crypto/pace
 make
