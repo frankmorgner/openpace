dc3c14fd18d736022dd6fc94acc6b504b49bd7df
frankmorgner <frankmorgner@bc35da7a-fd73-48a3-9038-9f319fca9165>
2009-11-08 17:41:59 +0000

    cleaned up patchset

--- make.sh
+++ make.sh
@@ -2,13 +2,14 @@
 
 # downloading and extracting
 wget http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz
-wget "http://marc.info/?l=openssl-dev&m=125730780821132&q=p3" -O ibm4.patch
+#wget "http://marc.info/?l=openssl-dev&m=125730780821132&q=p3" -O ibm4.patch
 tar -xzf openssl-1.0.0-beta3.tar.gz
 
 # datching
 cd openssl-1.0.0-beta3
 patch --strip 1 < ../ibm4.patch
-patch --strip 1 < ../openpace.patch
+patch --strip 1 < ../BP.patch
+patch --strip 1 < ../OpenPACE.patch
 
 # symbolic linking
 (cd include/openssl/ ;\
@@ -18,5 +19,3 @@ ln -v -s ../../crypto/cmac/cmac.h \
 # compile
 sleep 1
 ./config && make
-make -C crypto/pace
-make
