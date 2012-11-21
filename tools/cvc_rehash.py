9d7e5063d46bfcdf4b1e3fe208535248e14e3180
Dominik <oepen@informatik.hu-berlin.de>
2012-11-21 17:32:35 +0100

    Add license header to cvc_rehash.py

--- tools/cvc_rehash.py
+++ tools/cvc_rehash.py
@@ -1,3 +1,22 @@
+"""
+Copyright (c) 2010-2012 Dominik Oepen and Frank Morgner
+
+This file is part of OpenPACE.
+
+OpenPACE is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+OpenPACE is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+OpenPACE.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import os
 import sys
 
