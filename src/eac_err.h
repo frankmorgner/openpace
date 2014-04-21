frankmorgner/openpace

commit e4849bf77a9e6fa633a6546e0bf8a37e70462f0b

surround macros with braces
fixes unintended use in special cases

src/eac_err.h
@@ -40,9 +40,9 @@
 
 /* TODO: Make sure that ERR_load_crypto_strings() has been called */
 #define ossl_errors() ERR_print_errors_fp(stderr)
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d ) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__); ossl_errors()
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); goto err; }
-#define check_return(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; return NULL;}
+#define log_err(M, ...) {fprintf(stderr, "[ERROR] (%s:%d ) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__); ossl_errors()}
+#define check(A, M, ...) {if(!(A)) { log_err(M, ##__VA_ARGS__); goto err; }}
+#define check_return(A, M, ...) {if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; return NULL;}}
 
 
 #endif

commit f71be7e22df62a9794a9deaa84ab9ccac8e23cdb

fixed missing semi colon
regression from e4849bf77a9e6fa633a6546e0bf8a37e70462f0b

src/eac_err.h
@@ -40,7 +40,7 @@
 
 /* TODO: Make sure that ERR_load_crypto_strings() has been called */
 #define ossl_errors() ERR_print_errors_fp(stderr)
-#define log_err(M, ...) {fprintf(stderr, "[ERROR] (%s:%d ) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__); ossl_errors()}
+#define log_err(M, ...) {fprintf(stderr, "[ERROR] (%s:%d ) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__); ossl_errors();}
 #define check(A, M, ...) {if(!(A)) { log_err(M, ##__VA_ARGS__); goto err; }}
 #define check_return(A, M, ...) {if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; return NULL;}}
 