d9c24704d5fa1b231fde1eb64eed3e5e28fbc007
Frank Morgner <morgner@informatik.hu-berlin.de>
2014-09-04 17:27:40 +0200

    added missing read_file.h

---
+++ src/read_file.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2014 Frank Morgner
+ *
+ * This file is part of OpenPACE.
+ *
+ * OpenPACE is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * OpenPACE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OpenPACE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file read_file.h
+ *
+ * @author Frank Morgner <morgner@informatik.hu-berlin.de>
+ */
+
+/* FIXME correct header for size_t */
+#include <stdlib.h>
+
+int read_file(const char *filename, unsigned char **out, size_t *outlen);
