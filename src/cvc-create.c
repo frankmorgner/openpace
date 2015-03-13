frankmorgner/openpace

commit 0daa020696bf35522f4028c77baa71612ad256f5

cvc-create: fixed rsa keylength when derived from signer
fixes #19

src/cvc-create.c
@@ -33,6 +33,7 @@
 #include <eac/cv_cert.h>
 #include <openssl/bio.h>
 #include <openssl/err.h>
+#include <openssl/rsa.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -619,6 +620,11 @@ int main(int argc, char *argv[])
             term_key_ctx = EVP_PKEY_CTX_new(signer_key, NULL);
             if (!term_key_ctx
                     || !EVP_PKEY_keygen_init(term_key_ctx)
+                    || !(EVP_PKEY_type(signer_key->type) == EVP_PKEY_RSA
+                        /* RSA keys set the key length during key generation
+                         * rather than parameter generation! */
+                        && EVP_PKEY_CTX_set_rsa_keygen_bits(term_key_ctx,
+                            EVP_PKEY_bits(signer_key)))
                     || !EVP_PKEY_keygen(term_key_ctx, &term_key))
                 goto err;
 

commit 4c6d1ac7e000844b81419d580b40d70a7e50a8ef

cvc-create: fixed generating ec keys
regression from 0daa020696bf35522f4028c77baa71612ad256f5

src/cvc-create.c
@@ -618,13 +618,16 @@ int main(int argc, char *argv[])
         if (cmdline.sign_as_given) {
             term_key_ctx = EVP_PKEY_CTX_new(signer_key, NULL);
             if (!term_key_ctx
-                    || !EVP_PKEY_keygen_init(term_key_ctx)
-                    || !(EVP_PKEY_type(signer_key->type) == EVP_PKEY_RSA
-                        /* RSA keys set the key length during key generation
-                         * rather than parameter generation! */
-                        && EVP_PKEY_CTX_set_rsa_keygen_bits(term_key_ctx,
+                    || !EVP_PKEY_keygen_init(term_key_ctx))
+                goto err;
+            if (EVP_PKEY_type(signer_key->type) == EVP_PKEY_RSA) {
+                /* RSA keys set the key length during key generation
+                 * rather than parameter generation! */
+                if (!EVP_PKEY_CTX_set_rsa_keygen_bits(term_key_ctx,
                             EVP_PKEY_bits(signer_key)))
-                    || !EVP_PKEY_keygen(term_key_ctx, &term_key))
+                    goto err;
+            }
+            if (!EVP_PKEY_keygen(term_key_ctx, &term_key))
                 goto err;
 
             /* export key */