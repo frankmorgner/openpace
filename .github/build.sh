6444de0409c9b8e843f2d1efb5ac68b076be13c5
Frank Morgner <frankmorgner@gmail.com>
2023-07-19 15:07:47 +0200

    removed cross compile scripts

--- .github/build.sh
+++ .github/build.sh
@@ -12,9 +12,11 @@ case "$1" in
         ;;
     mingw-32)
         DEPS="$DEPS mingw-w64-tools binutils-mingw-w64-i686 gcc-mingw-w64-i686"
+        export ac_cv_func_malloc_0_nonnull=yes CC=i686-w64-mingw32-gcc AR=i686-w64-mingw32-ar RANLIB=i686-w64-mingw32-ranlib
         ;;
     mingw-64)
         DEPS="$DEPS mingw-w64-tools binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64"
+        export ac_cv_func_malloc_0_nonnull=yes CC=x86_64-w64-mingw32-gcc AR=x86_64-w64-mingw32-ar RANLIB=x86_64-w64-mingw32-ranlib
         ;;
 esac
 
@@ -34,6 +36,23 @@ case "$1" in
         ;;
 esac
 
+case "$1" in
+    mingw-32)
+        test -d openssl || git clone --depth=1 https://github.com/openssl/openssl.git -b OpenSSL_1_0_2-stable openssl
+        cd openssl
+        ./Configure no-asm no-shared mingw -DPURIFY -static-libgcc
+        make
+        cd ..
+        ;;
+    mingw-64)
+        test -d openssl || git clone --depth=1 https://github.com/openssl/openssl.git -b OpenSSL_1_0_2-stable openssl
+        cd openssl
+        ./Configure no-asm no-shared mingw64 -DPURIFY -static-libgcc
+        make
+        cd ..
+        ;;
+esac
+
 autoreconf -vis
 
 case "$1" in
@@ -41,9 +60,14 @@ case "$1" in
         export GCCGOFLAGS="-static-libgcc $CFLAGS"
         ./configure --enable-python --enable-java --enable-ruby --enable-go
         ;;
-    mingw-32|mingw-64|macos)
-        export GNUMAKEFLAGS=-j1
-        ./configure --enable-openssl-install
+    mingw-32)
+        ./configure --host=i686-w64-mingw32   CRYPTO_CFLAGS="-I$PWD/openssl/include" CRYPTO_LIBS="-I$PWD/openssl -lcrypto"
+        ;;
+    mingw-64)
+        ./configure --host=x86_64-w64-mingw32 CRYPTO_CFLAGS="-I$PWD/openssl/include" CRYPTO_LIBS="-I$PWD/openssl -lcrypto"
+        ;;
+    macos)
+        ./configure
         ;;
 esac
 
@@ -55,13 +79,7 @@ case "$1" in
         make distcheck
         sudo make uninstall
         ;;
-    mingw-32)
-        make win WIN_TOOL=i686-w64-mingw32
-        ;;
-    mingw-64)
-        make win WIN_TOOL=x86_64-w64-mingw32
-        ;;
-    macos)
-        make osx
+    mingw-32|mingw-64|macos)
+        make
         ;;
 esac
